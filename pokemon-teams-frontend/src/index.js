05e86e1727387e7cacc43413309f00bc319c5bda
hlbtyne <harriet.ballantyne@gmail.com>
2019-05-17 16:43:02 +0100

    Api created

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,18 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+const trainers = document.getElementById('trainers')
+
+function getData() {
+    return fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+}
+
+function renderOneTrainer() {
+    const trainerCard = document.createElement('div')
+    trainerCard.className = 'card'
+    trainerCard.innerHTML = `
+    
+    `
+}
